d00794f1a3a6d6fa932f350430367133cd82bf57
morfogeno <daniel.prof.ciencias@gmail.com>
2024-10-18 10:14:21 -0300

    atualização de corrida até o fim do tutorial workshop

--- make.R
+++ make.R
@@ -29,3 +29,6 @@ devtools::load_all(here::here())
 
 # List all R scripts in a sequential order and using the following form:
 # source(here::here("analyses", "script_X.R"))
+
+source(here::here("analyses", "01-clean.R"))
+source(here::here("analyses", "02-plot.R"))
